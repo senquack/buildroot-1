9ac05935db280e4b5dce939c3dee3599e938b4ba
Paul Cercueil <paul@crapouillou.net>
2019-04-20 00:36:42 +0200

    libini: Also install to staging directory
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- package/libini/libini.mk
+++ package/libini/libini.mk
@@ -5,5 +5,6 @@
 #############################################################
 LIBINI_VERSION = c3413da
 LIBINI_SITE = $(call github,pcercuei,libini,$(LIBINI_VERSION))
+LIBINI_INSTALL_STAGING = YES
 
 $(eval $(cmake-package))
