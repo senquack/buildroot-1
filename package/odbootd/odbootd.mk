aba4b7dd8207c5e1eff6332a08d921b03beff80e
Paul Cercueil <paul@crapouillou.net>
2019-04-19 19:52:24 +0200

    odbootd: Bump version
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- package/odbootd/odbootd.mk
+++ package/odbootd/odbootd.mk
@@ -4,7 +4,9 @@
 #
 ################################################################################
 
-ODBOOTD_VERSION = e919e44
+ODBOOTD_VERSION = fc4f329
 ODBOOTD_SITE = $(call github,opendingux,odbootd,$(ODBOOTD_VERSION))
+
+ODBOOTD_CONF_OPTS = -DWITH_ODBOOT_CLIENT:BOOL=OFF
 
 $(eval $(cmake-package))
